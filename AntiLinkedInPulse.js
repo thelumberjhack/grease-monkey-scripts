3d3e9077491583b8d2ffcaf886dc4086450a3430
Yannick Formaggio-Valentin <yannick.formaggio@gmail.com>
2014-04-30 07:19:15 +0200

    First commit: Anti LinkedIn Pulse script.

---
+++ AntiLinkedInPulse.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name          AntiLinkedInPulse
+// @namespace     http://yformagg.io
+// @description   This script removes annoying Linkedin Pulse news.
+// @include       https://www.linkedin.com/*
+// @include       https://*.linkedin.com/*
+// @include       http://linkedin.com/*
+// @include       http://*.linkedin.com/*
+// @version       1.0
+// @grant         none
+// ==/UserScript==
+
+GM_addStyle('#today-news-wrapper { display: none !important; }');
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ grease-monkey-scripts
 =====================
 
 My Firefox GreaseMonkey Scripts
+
+## Anti LinkedIn Pulse
+This script removes the annoying Pulse news block on LinkedIn home page.
